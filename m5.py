2093d0e55c6cf598415b768f83b74009fa8b9374
kajal-rawat0410 <2006kajalpauri@gmail.com>
2025-05-05 20:37:22 +0530

    Create m5.py

---
+++ m5.py
@@ -0,0 +1,10 @@
+import call
+a = int(input("enter number1:"))
+b = int(input("enter number2:"))
+
+print("Addition:", call.add1(a, b))
+print("Subtraction:", call.sub1(a, b))
+print("Multiplication:", call.mul1(a, b))
+print("Division:", call.div1(a, b))
+print("Modulus:", call.mod1(a, b))
+print("Power:", call.pow1(a, b))
